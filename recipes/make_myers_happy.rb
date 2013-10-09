d6aff780ea24a0dd21183e8c8c5d441fd70bd459
Myers Carpenter <myers@maski.org>
2013-10-09 11:31:00 -0400

    attempt to fix the joerc

--- recipes/make_myers_happy.rb
+++ recipes/make_myers_happy.rb
@@ -1,15 +1,26 @@
 package 'joe'
 
-python 'joe: enable nobackup' do
-  code <<-PYSCRIPT
+# python 'joe: enable nobackup' do
+#   code <<-PYSCRIPT
 
-import re
-ff = open('/etc/joe/joerc', 'rb')
-new_contents = re.sub('  -nobackup', '-nobackup', ff.read())
-ff = open('/etc/joe/joerc', 'wb')
-ff.write(new_contents)
+# import re
+# ff = open('/etc/joe/joerc', 'rb')
+# new_contents = re.sub('  -nobackup', '-nobackup', ff.read())
+# ff = open('/etc/joe/joerc', 'wb')
+# ff.write(new_contents)
 
-  PYSCRIPT
+#   PYSCRIPT
+# end
+
+ruby_block 'joe: enable nobackup' do
+  block do
+    rc = Chef::Util::FileEdit.new('/etc/joe/joerc')
+    rc.search_file_replace(
+      /  -nobackup/,
+      '-nobackup'
+    )
+    rc.write_file
+  end
 end
 
 if node[:lightnet][:user] == 'vagrant'
