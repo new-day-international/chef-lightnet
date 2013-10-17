f7a5237c6a53830018c17833955d5e4affabb790
Myers Carpenter <myers@maski.org>
2013-10-17 10:29:54 -0400

    another attempt to fix the problems with the wrong version of postgresql getting installed

--- recipes/reddit_package_repo.rb
+++ recipes/reddit_package_repo.rb
@@ -1,39 +1,15 @@
-# # aptitude configuration
-# APTITUDE_OPTIONS="-y" # limit bandwidth: -o Acquire::http::Dl-Limit=100"
-# export DEBIAN_FRONTEND=noninteractive
+apt_preference 'reddit' do
+  package_name '*'
+  pin          'release o=LP-PPA-reddit'
+  pin_priority '600'
+end
 
-# # add the reddit ppa for some custom packages
-# apt-get install $APTITUDE_OPTIONS python-software-properties
-# apt-add-repository -y ppa:reddit/ppa
-
-# These crazy ".run_action(:add)" are a way to get the ppa added to apt before
-# the postgresql recipe downloads the postgresql packages to build pg gem
 apt_repository "reddit" do
   uri "http://ppa.launchpad.net/reddit/ppa/ubuntu"
   distribution node['lsb']['codename']
   components ["main"]
-  # https://bugs.launchpad.net/ubuntu-website/+bug/435193
+  # why this keyserver? https://bugs.launchpad.net/ubuntu-website/+bug/435193
   keyserver "pool.sks-keyservers.net"
   key "65506D27"
-end.run_action(:add)
-
-# # pin the ppa -- packages present in the ppa will take precedence over
-# # ones in other repositories (unless further pinning is done)
-# cat <<HERE > /etc/apt/preferences.d/reddit
-# Package: *
-# Pin: release o=LP-PPA-reddit
-# Pin-Priority: 600
-# HERE
-
-file "/etc/apt/preferences.d/reddit" do
-  owner "root"
-  group "root"
-  mode "0755"
-  action :create
-  content <<-HERE
-Package: *
-Pin: release o=LP-PPA-reddit
-Pin-Priority: 600
-HERE
-end.run_action(:create)
-
+  cache_rebuild true
+end
