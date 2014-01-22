436c558669f9320e38d608a470213827910b2e1b
Myers Carpenter <myers@maski.org>
2014-01-22 16:17:36 -0500

    intended to be on the last commit.  Changes to recipes/nginx.rb

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -14,50 +14,24 @@ end
 #   key "http://nginx.org/keys/nginx_signing.key"
 #   cache_rebuild true
 # end
+# file "/etc/nginx/conf.d/default.conf" do
+#   action :delete
+# end
+# file "/etc/nginx/conf.d/example_ssl.conf" do
+#   action :delete
+# end
+# template "/etc/nginx/conf.d/lightnet.conf" do
+#   source "nginx-lightnet.erb"
+#   mode 0644
+# end
 
 apt_package 'nginx' do
   action :install
 end
 
-file "/etc/nginx/sites-available/lightnet" do
-  content <<-NGINX
-upstream app_server {
-    # server unix:/tmp/lightnet_gunicorn.sock fail_timeout=0;
-    # For a TCP configuration:
-    server 127.0.0.1:8001 fail_timeout=0;
-}
-
-server {
-    #{"sendfile off;" if node[:lightnet][:environment_type] == 'development'}
-
-    listen 80 default;
-    client_max_body_size 4G;
-    server_name _;
-
-    keepalive_timeout 5;
-
-    # path for static files
-    root #{node[:lightnet][:application_directory]}/reddit/r2/r2/public;
-
-    location / {
-        # checks for static file, if not found proxy to app
-        try_files $uri @proxy_to_app;
-    }
-
-    location @proxy_to_app {
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Host $http_host;
-        proxy_redirect off;
-
-        proxy_pass http://app_server;
-    }
-
-    error_page 500 502 503 504 /500.html;
-    location = /500.html {
-        root #{node[:lightnet][:application_directory]}/reddit/r2/r2/public;
-    }
-}
-  NGINX
+template "/etc/nginx/sites-available/lightnet" do
+  source "nginx-lightnet.erb"
+  mode 0644
 end
 
 file "/etc/nginx/sites-enabled/default" do
